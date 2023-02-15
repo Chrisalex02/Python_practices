8f9a8fc8405f5df64714397a88841ec7cc0cd40f
Christian Martinez <christianalexandermartinez02@gmail.com>
2023-02-15 12:56:13 +0000

    añadidas indicaciones para extraer las dependencias

--- streamlit/get-started/uber_pickups.py
+++ streamlit/get-started/uber_pickups.py
@@ -59,3 +59,6 @@ filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 st.subheader(f'Map of all pickups at {hour_to_filter}:00')
 st.map(filtered_data)
 
+#Asi se consiguen las librerias para el requerimets.txt
+#pip freeze > requirements.txt
+#cat requirements.txt
