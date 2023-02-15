418767f3ca03f73a890b6fc94ab384cdfd0fb434
Christian Martinez <christianalexandermartinez02@gmail.com>
2023-02-15 12:07:13 +0000

    prueba con streamlit

---
+++ streamlit/get-started/uber_pickups.py
@@ -0,0 +1,7 @@
+#importando librerias basicas
+import streamlit as st #Para desplegar y formatear la app
+import pandas as pd #Para manipular datos
+import numpy as np #Para operaciones de algebra lineal
+
+st.title('Uber pickups in NYC') #titulo de la app
+
